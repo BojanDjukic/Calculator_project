6df5a6af343c145af0fd626d6df933bed8fb346e
Bojan Djukic <bdjukic@domain.local>
2019-07-07 22:45:33 +0200

    Initial commit, adding .py template

---
+++ src/Calculator.py
@@ -0,0 +1,19 @@
+
+"""
+Name
+   Calcuator.py
+
+Purpose
+    This python script is used to create Calcuator, using Tkinter module
+    
+Revision Dates
+    07-Jul-2019 [0.1]: Initial Version
+
+
+"""
+
+__authors__    = ["Julian Sony", "Bojan Djukic"]
+__version__    = "0.1"
+__maintainer__ = ""
+
+__status__     = "Development"
